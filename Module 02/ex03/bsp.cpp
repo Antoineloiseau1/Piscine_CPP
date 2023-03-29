8ce4d9a3428ae73092e952e33ffeeb31ebff44bc
Antoineloiseau1 <antoineloiseau1@gmail.com>
2023-03-29 12:03:46 +0200

    Module02 Done

--- Module 02/ex03/bsp.cpp
+++ Module 02/ex03/bsp.cpp
@@ -1,17 +1,26 @@
 #include "Point.hpp"
 
-float	calculateAera(Point const a, Point const b, Point const c) {
-	
+Fixed	calculateArea(Point const a, Point const b, Point const p) {
+	/* Calculates the area of a parallelogram given by points a, b and p*/
+	const Fixed aX = a.getX(), aY = a.getY();
+	const Fixed bX = b.getX(), bY = b.getY();
+	const Fixed pX = p.getX(), pY = p.getY();
+	/* this calculates the determinent of the matrice of vectors (p, a) and (p, b) */
+	return ((aX - pX) * (bY - pY)) - ((aY - pY) * (bX - pX));
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point) {
-	/*calculate aera of triangle
-		area = 1/2(ab + ac + bc) */
+	/* Chech if point is on the right or left side ofthe line of each sommets*/
+	Fixed area1 = (calculateArea(a, b, point));
+	Fixed area2 = (calculateArea(b, c, point));
+	Fixed area3 = (calculateArea(c, a, point));
+	/* if a b and c are clockwise*/
+	if (area1 > 0 && area2 > 0 && area3 > 0)
+		return true;
+	/* if a b and c are counter clockwise*/
+	else if (area1 < 0 && area2 < 0 && area3 < 0)
+		return true;
+	else
+		return false;
 
-	#calculate area of sub triangles
-
-	#if sum of all sub triangles == area of triangle
-		#return True
-	#else
-		#return False
 }
--- Module 02/ex03/main.cpp
+++ Module 02/ex03/main.cpp
@@ -1,12 +1,58 @@
-#include <iostream>
 #include "Point.hpp"
 
 int main( void ) {
 
-	Point a(3.58, 12.15);
-	Point b(1.25, 42.42);
-	Point c(-6.98, -4.52);
-	Point point(2.36, 8.75);
-	std::cout << bsp(a, b, c, point) << std::endl;
+	{
+		/* Point Inside */
+		std::cout << "/* Point Inside */" << std::endl;
+		Point a(1, 2);
+		Point b(3, 4);
+		Point c(5, 2);
+		Point point(3, 3);
+		std::cout << "a" << a << "\nb" << b << "\nc" << c << "\npoint" << point << std::endl;
+		std::cout << ((bsp(a, b, c, point) == true) ? "true" : "false") << std::endl << std::endl;
+	}
+	{
+		/* Point Outside */
+		std::cout << "/* Point Outside */" << std::endl;
+		Point a(1, 2);
+		Point b(-1, -1);
+		Point c(5, 2);
+		Point point(3, 3);
+		std::cout << "a" << a << "\nb" << b << "\nc" << c << "\npoint" << point << std::endl;
+		std::cout << ((bsp(a, b, c, point) == true) ? "true" : "false") << std::endl << std::endl; 
+	}
+	{
+		/* Point On side */
+		std::cout << "/* Point On side */" << std::endl;
+		Point a(1, 2);
+		Point b(3, 4);
+		Point c(5, 2);
+		Point point(3, 2);
+		std::cout << "a" << a << "\nb" << b << "\nc" << c << "\npoint" << point << std::endl;
+		std::cout << ((bsp(a, b, c, point) == true) ? "true" : "false") << std::endl << std::endl; 
+	}
+	{
+		/* Point Equals b */
+		std::cout << "/* Point Equals b */" << std::endl;
+		Point a(1, 2);
+		Point b(3, 4);
+		Point c(5, 2);
+		Point point(3, 4);
+		std::cout << "a" << a << "\nb" << b << "\nc" << c << "\npoint" << point << std::endl;
+		std::cout << ((bsp(a, b, c, point) == true) ? "true" : "false") << std::endl << std::endl; 
+	}
+	{
+		/* float values*/
+		std::cout << "/* float values */" << std::endl;
+		Point a(1.4568, 2.78912);
+		Point b(3.7568, 4.12678);
+		Point c(5.78531, 2.1264);
+		Point point(3.1248, 2.78621);
+		std::cout << "a" << a << "\nb" << b << "\nc" << c << "\npoint" << point << std::endl;
+		std::cout << ((bsp(a, b, c, point) == true) ? "true" : "false") << std::endl; 
+	}
+	
+
 	return 0;
 }
