7f825d12b90d98c7c291ceea307a04579ceba6ec
antoine <antoineloiseau1@gmail.com>
2023-04-19 15:52:26 +0200

    M6E1

--- Module 06/ex01/main.cpp
+++ Module 06/ex01/main.cpp
@@ -3,10 +3,13 @@
 
 int main(void) {
 	Data d = {65, 3.14, "Hello, World!"};
-	std::cout << "Data struct: " << d << std::endl << "its address is: " << &d << std::endl;
+	std::cout << "Data struct: " << d << std::endl << "Its address is: " << &d << std::endl << std::endl;
+
 	unsigned int*	ptr = Serializer::serialize(&d);
 	std::cout << "return value of serialize: " << ptr << std::endl;
+
 	Data* dptr = Serializer::deserialize(ptr);
-	std::cout << "return value of deserialize: "<< *dptr << std::endl;
-	std::cout << "with adress: " << dptr << std::endl;
+	std::cout << "return value of deserialize: "<< *dptr << std::endl << "with adress: " << dptr << std::endl;
+	
+	std::cout << std::endl << "chained: " << *Serializer::deserialize(Serializer::serialize(&d)) << std::endl;
 } 
