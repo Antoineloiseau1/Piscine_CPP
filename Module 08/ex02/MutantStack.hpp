cf0f64d01ac5bf36d753514dc64d22123737f44f
Antoineloiseau1 <antoineloiseau1@gmail.com>
2023-09-27 10:33:33 +0200

    iterator works

--- Module 08/ex02/MutantStack.hpp
+++ Module 08/ex02/MutantStack.hpp
@@ -3,103 +3,45 @@
 
 # include <stack>
 # include <deque>
-# include <cstddef>
-# include <iostream>
 
-template <typename T, typename U = std::deque<T> >
-class MutantStack {
-
-	private:
-
-		std::stack<T, U>	_stack;
+template <typename T, class Container = std::deque<T> >
+class MutantStack: public std::stack<T, Container> 
+{
 
 	public:
 
-		// Other type of container as construction parameter
-		template<typename V>
-		MutantStack<T, U>(const V & container) : _stack(container) {}
-
-		// Canonical Form Class Constructors
-		MutantStack<T, U>(void) {}
-		MutantStack<T, U>(const MutantStack & src) { /* insert code here */ }
-		const MutantStack &	operator=(const MutantStack & rhs) {/* Insert code here */ }
-		~MutantStack<T, U>(void) {}
-
-		// Stack based functions:
-		bool	empty(void)						const	{ return _stack.empty(); }
-		size_t	size(void)						const	{ return _stack.size(); }
-		T		top(void)						const	{ return _stack.top(); }
-		void	push(T element)							{ _stack.push(element); }
-		void	pop(void)								{ _stack.pop(); }
-		void	swap(const MutantStack & stack)			{ _stack.swap(stack); }
-	
-		// Creating an iterator to MutantStack class:
-		class iterator : std::iterator<std::input_iterator_tag, T>
-		{
-			private:
-
-				T*				_p;
-				T*				_getPointer(void) const { return _p; }
-
-			public:
-
-				// Canonical form class functions
-				iterator(void) {}
-				iterator(T* addr): _p(addr) {}
-				iterator(const iterator & src) :  _p(src._getPointer()) {}
-
-				// Operators overloads functions
-				iterator&	operator=(const T & rhs) 
-				{
-					if(this == &rhs)
-					{
-						return *this;
-					}
-					return &rhs; 
-				}
-				T&			operator*(void) const { return *_p; }
-				bool		operator==(const iterator & rhs) const { return _p == rhs._getPointer(); }
-				bool		operator!=(const iterator & rhs) const { return _p != rhs._getPointer(); }
-				iterator&	operator++(void) { --_p; return *this; }
-				iterator	operator++(int) 
-				{
-					iterator	tmp = *this;
-
-					operator++();
-					return tmp;
-				}
-				iterator&	operator--(void) { ++_p; return *this; }
-				iterator	operator--(int) 
-				{
-					iterator	tmp = *this;
-
-					operator--();
-					return tmp;
-				}
-		};
-		
-		// Pointers accessors
-		iterator	begin(void) { return iterator(&_stack.top()); }
-
-		iterator	end(void)	{
-
-			std::stack<int>	tmp;
-			// int					value;
-			// std::vector<T>		vector;
-			MutantStack::iterator	it;
-
-			std::swap(tmp, _stack);
-			// it = &_stack.top();
-			// this->swap(tmp);
-			// // while(!tmp.empty())
-			// {
-			// 	value = tmp.top();
-
-
-			// }
-			
-			return	iterator(it);
+		MutantStack(void) {}
+		MutantStack(const MutantStack &src) 
+		{ 
+			for (iterator it = src.begin(); it != src.end(); it++) 
+			{
+				push(*it); 
+			}
 		}
+		~MutantStack(void) {}
+
+		MutantStack &	operator=(const MutantStack &rhs) 
+		{
+			if(this != &rhs)
+			{
+				std::stack<T, Container>().swap(this);
+				for (iterator it = rhs.begin(); it != rhs.end(); it++) 
+				{
+					push(*it);
+				}
+			}
+			return *this;
+		}
+
+		/*	- typename keyword tell the compiler the next sentence is considered as a type
+			- here, the type is std::stack<T, Container>::container_type::iterator
+			- container_type is referred to  the underlying container of std::stack<T, Container>
+			- typedef creates an "alias" named "iterator" to the whole line */
+		typedef typename std::stack<T, Container>::container_type::iterator iterator;
+
+		//this->c is a pointer to c (underlying container)
+		iterator	begin() { return this->c.begin(); }
+		iterator	end() { return this->c.end(); }
 
 };
 
