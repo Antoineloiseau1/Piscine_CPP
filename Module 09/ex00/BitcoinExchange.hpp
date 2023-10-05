894ffac4240d428e71843a043075706154f5ca6b
Antoineloiseau1 <antoineloiseau1@gmail.com>
2023-10-05 15:18:25 +0200

    work in progress on module 09

--- Module 09/ex00/BitcoinExchange.hpp
+++ Module 09/ex00/BitcoinExchange.hpp
@@ -5,20 +5,31 @@
 # include <iostream>
 # include <map>
 # include <sstream>
+# include <time.h>
+
+
+time_t	str_to_time(std::string str)
+{
+	struct tm	tm;
+	strptime(str.c_str(), "%Y-%m-%d", &tm);
+	return mktime(&tm);
+}
 
 template<typename T, typename U>
-std::map<T, U>	fill_map_from_file(std::ifstream & file, char separator)
+std::multimap<T, U>	fill_map_from_file(std::ifstream & file, char separator)
 {
-	std::map<T, U>	data;
-	std::string		line;
-	T				key;
-	U				value;
+	std::multimap<T, U>	data;
+	std::string			line;
+	T					key;
+	U					value;
 
+	// >>>>> Error Parsing Handling HERE <<<<
 	std::getline(file,line); // ignore first line
 	while (std::getline(file, line))
 	{
 		std::istringstream iss(line);
-		iss >> key >> separator >> value;
+		std::getline(iss, key, separator);
+		iss >> value;
 		data.insert(std::pair<T, U>(key, value));
 	}
 	return data;
--- Module 09/ex00/main.cpp
+++ Module 09/ex00/main.cpp
@@ -1,28 +1,47 @@
 #include "BitcoinExchange.hpp"
 
+template<typename T>
+void	displayMap(T & data)
+{
+	typename	T::iterator it;
+	for (it = data.begin(); it != data.end(); it++)
+	{
+		std::cout << "[" << it->first << "] = " << it->second << std::endl;
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	if(argc != 2)
+	if (argc != 2)
 	{
 		std::cerr << "usage: btc <file>\n";
 		return 1;
 	}
 
 	std::ifstream	inputFile(argv[1]);
+	std::ifstream	dataBase("data.csv");
 
-	if(!inputFile.is_open())
+	if (!inputFile.is_open() || ! dataBase.is_open())
 	{
 		std::cerr << "Error: failed to open file\n";
 		return 1;
 	}
 
-	std::map<std::string, float>	input;
-	input = fill_map_from_file<std::string, float>(inputFile, '|');
-	std::map<std::string, float>::iterator it;
-	for(it = input.begin(); it != input.end(); it++)
-	{
-		std::cout << "[" << it->first << "] = " << it->second << std::endl;
-	}
+	std::multimap<std::string,  float>	data = fill_map_from_file<std::string, float>(dataBase, ',');
+	dataBase.close();
+	std::multimap<std::string,  float>	input = fill_map_from_file<std::string, float>(dataBase, '|');
 	inputFile.close();
+	displayMap(input);
+	(void)data;
+	std::multimap<std::string, float>::iterator	it;
+	for (it = input.begin(); it != input.end(); it++)
+	{
+		std::cout << it->second << std::endl;
+		if (!(it->second >= 0 && it->second <= 1000))
+		{
+			std::cout << "Error: not a positive number\n";
+			it++;
+		}
+	}
 	return 0;
 }
